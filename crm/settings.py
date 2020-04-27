f6b73aa248cb657bab4b1eaeeb7d6e9f4b0456cd
Thaddeus <thaddeuscleo@gmail.com>
2020-04-27 19:19:57 +0700

    created view on accounts app

--- crm/settings.py
+++ crm/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Created APP Named Account
+    'accounts',
 ]
 
 MIDDLEWARE = [
